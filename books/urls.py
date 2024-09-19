42efc05bfc7e752acbe1c122fc34c6911dc80eec
katakinaa <suiutaevalia@gmail.com>
2024-09-19 12:33:07 +0600

    CBV - CLASS BASED VIEWS + SEACH

--- books/urls.py
+++ books/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book_list/', views.book_list_view),
-    path('book_list/<int:id>/', views.book_detail_view),
     path('aboutme/', views.about_me),
     path('aboutmyfriend/', views.about_my_friend),
     path('thistime/', views.this_time),
+
+    path('book_list/', views.BookListView.as_view()),
+    path('book_list/<int:id>/', views.BookDetailView.as_view()),
+    path('create_book/', views.BookCreateView.as_view(), name='add_book'),
+    path('book_list/<int:id>/update/', views.BookEditView.as_view()),
+    path('book_list_edit/', views.BookUpdateListView.as_view(), name='book_list_edit'),
+    path('book_list/<int:id>/delete/', views.BookDropDeleteView.as_view()),
+    path('book_list_delete/', views.BookListDeleteView.as_view(), name='book_list_delete'),
+    path('search/', views.SearchView.as_view(), name='search'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,31 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from datetime import datetime
-from . import models
-
-
-def book_detail_view(request, id):
-    if request.method == 'GET':
-        book_id = get_object_or_404(models.Book, id=id)
-        return render(
-            request,
-            template_name='book_detail.html',
-            context={
-                'book_id': book_id
-            }
-        )
-
-
-def book_list_view(request):
-    if request.method == "GET":
-        book_object = models.Book.objects.all()
-        return render(
-            request,
-            template_name='book_list.html',
-            context={
-                'book_object': book_object
-            }
-        )
+from django.views import generic
+from . import models, forms
 
 
 def about_me(request):
@@ -38,3 +15,82 @@ def about_my_friend(request):
 
 def this_time(request):
     return HttpResponse(datetime.now().strftime('<h1>%H:%M:%S</h1>'))
+
+
+class BookListView(generic.ListView):
+    template_name = 'book_list.html'
+    context_object_name = 'book_object'
+    model = models.Book
+
+    def get_queryset(self):
+        return models.Book.objects.all()
+
+
+class BookDetailView(generic.DetailView):
+    template_name = 'book_detail.html'
+    context_object_name = 'book_id'
+
+    def get_object(self, **kwargs):
+        book_id = self.kwargs.get('id')
+        return get_object_or_404(models.Book, id=book_id)
+
+
+class BookCreateView(generic.CreateView):
+    template_name = 'crud/create_book.html'
+    form_class = forms.BookForm
+    success_url = '/book_list/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(BookCreateView, self).form_valid(form=form)
+
+
+class BookUpdateListView(generic.ListView):
+    template_name = 'crud/book_list_edit.html'
+    context_object_name = 'book_object'
+    model = models.Book
+
+    def get_queryset(self):
+        return self.model.objects.all().order_by('-id')
+
+
+class BookEditView(generic.UpdateView):
+    template_name = 'crud/update_book.html'
+    form_class = forms.BookForm
+    success_url = '/book_list_edit/'
+
+    def get_object(self, **kwargs):
+        book_id = self.kwargs.get('id')
+        return get_object_or_404(models.Book, id=book_id)
+
+
+class BookListDeleteView(generic.ListView):
+    template_name = 'crud/book_list_delete.html'
+    context_object_name = 'book_object'
+    model = models.Book
+
+    def get_queryset(self):
+        return self.model.objects.all().order_by('-id')
+
+
+class BookDropDeleteView(generic.DeleteView):
+    template_name = 'crud/confirm_delete.html'
+    success_url = '/book_list_delete/'
+
+    def get_object(self, **kwargs):
+        book_id = self.kwargs.get('id')
+        return get_object_or_404(models.Book, id=book_id)
+
+
+class SearchView(generic.ListView):
+    template_name = 'book_list.html'
+    context_object_name = 'book_object'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return models.Book.objects.filter(title__icontains=self.request.GET.get('q')).order_by('-id')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
